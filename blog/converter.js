91aac42b18c49e1f222814786250467c878ee8cf
FedG-code <federico.giovannini28@gmail.com>
2022-09-21 13:05:34 +0100

    Improvements
    Better text formatting with margin usage. A header that at the moment doesn't take up the full page. A broken progress bar, I think my script might be in the wrong spot?

--- blog/converter.js
+++ blog/converter.js
@@ -1,19 +1,37 @@
 const {readdirSync, readFileSync, writeFileSync} = require('fs');
-
 const showdown = require('showdown');
-
 const convertToHTML = new showdown.Converter();
+const Handlebars = require("handlebars");
 
+//get the list of markdown blogs
 const listOfMarkdownFiles = readdirSync('markdown');
 
 console.log(listOfMarkdownFiles);
 
+//generate the template to apply to the files
+const template = Handlebars.compile(readFileSync('template.hbs', 'utf8'));
+
 listOfMarkdownFiles.forEach(file=>{
     const fileContent = readFileSync(`markdown/${file}`, 'utf8');
-   ///Find and remove the pesky invisible character that messes the first title formatting
-    const formattedFile = (fileContent.replace('﻿#', '#'));
-   //pass the file through the converter
-   const newHTMLFile = convertToHTML.makeHtml(formattedFile);
-   //write the html file with its new name
-   writeFileSync(file.replace('.md', '.html'), newHTMLFile);
+    //Find and remove the pesky invisible character that messes the first title formatting
+    const readableFile = (fileContent.replace('﻿#', '#'));
+    //pass the file through the converter
+    var newHTMLFile = convertToHTML.makeHtml(readableFile);
+    //separate title from body
+    const jsHeaderStart = '<h1';
+    const jsHeaderEnd = '</h1';
+    const titleStart = newHTMLFile.search(jsHeaderStart);
+    const titleEnd = newHTMLFile.search(jsHeaderEnd);
+    var titleFull = newHTMLFile.substring(titleStart, titleEnd);
+    //remove the header formatting
+    titleFull = titleFull.replace(jsHeaderStart, '');
+    titleFull = titleFull.replace(jsHeaderEnd, '');
+    
+    const handlebarsInput = {title: titleFull, mainContent: newHTMLFile};
+
+
+    //apply template
+    const templatedFile = template(handlebarsInput);
+    //write the html file with its new name
+    writeFileSync(file.replace('.md', '.html'), templatedFile);
 });
